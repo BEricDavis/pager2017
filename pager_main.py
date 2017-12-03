3e40b7ccd48cdaa59724d3d45c041422883da587
B Eric Davis <feralmonkey@gmail.com>
2017-12-03 16:39:22 -0500

    Read in json config file

--- pager_main.py
+++ pager_main.py
@@ -2,8 +2,30 @@
 # This is the main script for creating a PagerDuty report
 
 import logging
+import json
 import argparse
 import datetime as dt
 from collections import defaultdict
 
-# read a config file to retrieve the domain
+log_level = 'DEBUG'
+def main():
+    configure_logging(log_level)
+    load_config()
+
+def configure_logging(level):
+    logging.getLogger('requests').setLevel(logging.CRITICAL)
+    logging.basicConfig(level=level,
+                        format='%(asctime)s %(levelname)s %(funcName)s: %(lineno)d %(message)s')
+
+def load_config():
+    # read a config file to retrieve the domain and the schedules
+    with open('conf/pd_info.json') as config:
+        config_json = json.load(config)
+        URL = config_json['Config']['URL']
+        services = config_json['Service']
+
+    logging.debug(f'URL: {URL}')
+    logging.debug(f'services: {services}')
+
+if __name__ == '__main__':
+    main()
